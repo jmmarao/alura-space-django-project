c21cdd9b0c8da6c7dfb002751445c98e6483da46
Jones <jmmarao@gmail.com>
2023-04-02 21:21:06 -0300

    Add index and imagem views
    Render static styles on templates

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from gallery.views import index
+from gallery.views import index, imagem
 
 urlpatterns = [
     path('', index),
+    path('imagem/', imagem)
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'gallery/index.html')
+
+def imagem(request):
+    return render(request, 'gallery/imagem.html')
